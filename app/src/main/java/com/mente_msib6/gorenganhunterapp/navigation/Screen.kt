bfeef1590cfaa551f001064289244dd1e0c18d3a
Hamdan Syaputra <hamdansyaputra02@gmail.com>
2024-05-12 17:50:24 +0700

    NewOrder1

--- app/src/main/java/com/mente_msib6/gorenganhunterapp/navigation/Screen.kt
+++ app/src/main/java/com/mente_msib6/gorenganhunterapp/navigation/Screen.kt
@@ -6,5 +6,26 @@ sealed class Screen (val route: String, val title: String){
     data object Profil: Screen("Profil","Profil")
     data object Detail: Screen("detail","Detail")
     data object DetailPenjual: Screen("detailPenjual","DetailPenjual")
+    companion object {
+        fun LocationRoute(route: String?): Screen {
+            return when (route) {
+                Home.route -> Home
+                List.route -> List
+                Profil.route -> Profil
+                Detail.route -> Detail
+                DetailPenjual.route -> DetailPenjual
+                else -> {
+                    if (route?.startsWith("detail/") == true){
+                        Detail
+                    }
+                    else if (route?.startsWith("detailPenjual/") == true){
+                        DetailPenjual
+                    } else {
+                        throw IllegalArgumentException("nope")
+                    }
+                }
 
+            }
+        }
+    }
 }
